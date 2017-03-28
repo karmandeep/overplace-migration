4ae4464d09f667976521837207e67344717c37c4
karmandeep <karmandeep.singh@gmail.com>
2017-03-28 15:46:17 +0530

    Added code to export clients, orders and ordered products

--- includes/database-migration2.php
+++ includes/database-migration2.php
@@ -2,14 +2,41 @@
 
 	//Now once the products have been migrated. Lets add the order/ordini and order_prodotti/tblhosting.
 	//Get the orders.
-	
+	echo '<pre>';
 	//customer_id for the corresponding order-id
-	//select o.*, u.* from ordini o,  utenti u where u.id = o.id_utente order by o.id; 
-	//select o.* , op.*, u.*  from ordini o, ordini_prodotti op, utenti u where o.id = op.id_ordine and u.id = o.id_utente order by o.id;
 	//Make Array of this.
+	$export_data_sql_array = array();
 	
+	//Let us first get the clients.
+	$export_client_sql = "select * from utenti";
+	$export_client_query = mysqli_query($dbexport_con , $export_client_sql); 
+	
+	//$cnt = 0;
+	while($export_client_data = mysqli_fetch_array($export_client_query , MYSQLI_ASSOC)) {
+		$customer_order_data = array();
+		$customer_orders_query = mysqli_query($dbexport_con , "select * from ordini where id_utente = '" . $export_client_data['id'] . "'");
 		
+		while($customer_orders = mysqli_fetch_array($customer_orders_query , MYSQLI_ASSOC)) {
+			
+			$customer_product_order_data = array();		
+			//Lets Get the Ordered products.
+			$customer_product_orders_query = mysqli_query($dbexport_con , "select * from ordini_prodotti where id_ordine = '" . $customer_orders['id'] . "'");
+			while($customer_product_orders = mysqli_fetch_array($customer_product_orders_query , MYSQLI_ASSOC)) {
+				$customer_product_order_data[] = $customer_product_orders;
+			}
+			//print_r($customer_orders);
+			$customer_order_data[] = array('order' => $customer_orders, 'ordered_products' => $customer_product_order_data);
+		}
+		
+		$export_data_sql_array[] = array('customerdetails' => $export_client_data, 'orders' => $customer_order_data);
+	}
+	
+	print_r($export_data_sql_array);
 
+	
+	//echo 1;
+	
+	exit;
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
